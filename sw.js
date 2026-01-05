ea28110716e625a1ab93c6a647106201d4e124cb
Tatsuya1881 <102351101+Tatsuya1881@users.noreply.github.com>
2026-01-05 15:16:33 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,18 @@
-const CACHE_NAME = 'molkky-v3';
-const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './apple-touch-icon.png'
+const CACHE_NAME = 'molkky-v8';
+const assets = [
+  './', './index.html', './style.css', './app.js', './manifest.json', './apple-touch-icon.png'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)));
+  }));
 });
